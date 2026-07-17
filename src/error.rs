c9fca1f9f2a2fdf6f8b236345040afa8368748cf
Alex Developer <alex@example.com>
2026-07-18 02:15:23 +0300

    feat: add reqwest error conversion to Error type

--- src/error.rs
+++ src/error.rs
@@ -16,11 +16,20 @@ pub enum ErrorKind {
 pub struct Error {
     pub kind: ErrorKind,
     pub message: String,
+    source: Option<Box<dyn std::error::Error + Send + Sync>>,
 }
 
 impl Error {
     pub fn new(kind: ErrorKind, message: impl Into<String>) -> Self {
-        Error { kind, message: message.into() }
+        Error { kind, message: message.into(), source: None }
+    }
+
+    pub fn with_source(
+        kind: ErrorKind,
+        message: impl Into<String>,
+        source: Box<dyn std::error::Error + Send + Sync>,
+    ) -> Self {
+        Error { kind, message: message.into(), source: Some(source) }
     }
 }
 
@@ -30,6 +39,22 @@ impl fmt::Display for Error {
     }
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.source.as_ref().map(|s| s.as_ref() as &(dyn std::error::Error + 'static))
+    }
+}
+
+impl From<reqwest::Error> for Error {
+    fn from(e: reqwest::Error) -> Self {
+        Error::with_source(ErrorKind::Http, "HTTP request failed", Box::new(e))
+    }
+}
+
+impl From<url::ParseError> for Error {
+    fn from(e: url::ParseError) -> Self {
+        Error::with_source(ErrorKind::InvalidUrl, "Invalid URL", Box::new(e))
+    }
+}
 
 pub type Result<T> = std::result::Result<T, Error>;
